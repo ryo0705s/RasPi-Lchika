eaa851e05df31552bd708ba810e690564f2b8b3d
ryo0705s <inspire.sasaki15@gmail.com>
2024-09-15 23:05:49 +0900

    Grioのポートを27に変えても付くかチェック2

--- led.py
+++ led.py
@@ -12,13 +12,13 @@ GPIO.setup(27, GPIO.OUT)
 
 while True:
   # GPIO17の出力を1にして、LED点灯
-  GPIO.output(17, 1)
+  GPIO.output(27, 1)
 
   # 0.5秒待つ
   time.sleep(0.5)
 
   # GPIO17の出力を0にして、LED消灯
-  GPIO.output(17, 0)
+  GPIO.output(27, 0)
 
   # 0.5秒待つ
   time.sleep(0.5)
